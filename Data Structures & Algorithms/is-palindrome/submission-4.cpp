8f94847f1c9fcb71686366a936b22f8aebfa4b48
Atharv Randive <149844530+DarkCarnage06@users.noreply.github.com>
2026-06-06 15:19:27 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0;
+        int end=s.size()-1;
+        while(start<end){
+            if(!iswalnum(s[start])){
+                start++;
+                continue;
+            }
+            if(!iswalnum(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end])){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};

c96ff1f12fa8221f43fa3f284bab27a150d1b5f8
Atharv Randive <149844530+DarkCarnage06@users.noreply.github.com>
2026-09-18 11:32:44 +0530

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+       
+        if(nums.empty())return 0;
+        sort(nums.begin(),nums.end());
+
+        int longest=1;
+        int count=1;
+        int smaller=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==smaller+1){
+                count++;
+                smaller=nums[i];
+            }else if(nums[i]==smaller){
+                continue;
+            }else{
+                count=1;
+                smaller=nums[i];
+            }
+            longest=max(longest,count);
+        }
+        return longest;
+    }
+};
